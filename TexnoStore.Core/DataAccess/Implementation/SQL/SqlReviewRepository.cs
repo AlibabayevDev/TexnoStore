93a603321595a3b8bb65510d0ce887ac97a150a1
Nahid Mirzazada <Admin@NAHIDPC>
2022-08-03 18:48:24 +0400

    review repository added

---
+++ TexnoStore.Core/DataAccess/Implementation/SQL/SqlReviewRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TexnoStore.Core.DataAccess.Abstract;
+using TexnoStore.Core.Domain.Entities;
+
+namespace TexnoStore.Core.DataAccess.Implementation.SQL
+{
+    public class SqlReviewRepository : IReviewRepository
+    {
+        public  bool Add(Review review)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.Open();
+
+                string cmdText = "Insert into Review values(@Name,@Email,@Message,@StarCount)";
+
+                using (SqlCommand cmd = new SqlCommand(cmdText, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Name", review.Name);
+                    cmd.Parameters.AddWithValue("@Email", review.Email);
+                    cmd.Parameters.AddWithValue("@Message", review.Message);
+                    cmd.Parameters.AddWithValue("@StarCount", review.StarCount);
+
+                    int affectedCount = cmd.ExecuteNonQuery();
+
+                    return affectedCount == 1;
+                }
+            }
+        }
+    }
+}
---
+++ TexnoStore/Models/ReviewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TexnoStore.Models
+{
+    public class ReviewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        [Required]
+        public string StarCount { get; set; }
+    }
+}
