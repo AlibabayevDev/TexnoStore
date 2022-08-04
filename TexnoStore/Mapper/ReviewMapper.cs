1e7fcde48e210bca07c20cff01ffef59c76139f2
Nahid Mirzazada <Admin@NAHIDPC>
2022-08-04 15:52:47 +0400

    review controller added

---
+++ TexnoStore/Mapper/ReviewMapper.cs
@@ -0,0 +1,33 @@
+﻿using TexnoStore.Core.Domain.Entities;
+using TexnoStore.Models;
+
+namespace TexnoStore.Mapper
+{
+    public class ReviewMapper
+    {
+        public Review Map(ReviewModel model)
+        {
+            return new Review()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Email = model.Email,
+                Message = model.Message,
+                StarCount = model.StarCount,
+            };
+        }
+
+
+        public ReviewModel Map(Review entity)
+        {
+            return new ReviewModel()
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Email = entity.Email,
+                Message = entity.Message,
+                StarCount = entity.StarCount,
+            };
+        }
+    }
+}
