cab1e8e5639eb7e1d948c861068bd51d90727f75
Nahid Mirzazada <Admin@NAHIDPC>
2022-08-08 15:29:18 +0400

    .

--- TexnoStore/Controllers/LaptopController.cs
+++ TexnoStore/Controllers/LaptopController.cs
@@ -115,7 +115,7 @@ namespace TexnoStore.Controllers
             return LaptopProduct(viewModel.Laptop.Id);
         }
 
-        public override IActionResult ShopCart(LaptopListViewModel viewModel)
+        public IActionResult ShopCart(LaptopListViewModel viewModel)
 		{
             var name=User.Identity.Name;
             var userid = userManager.FindByNameAsync(name).Result;
@@ -131,67 +131,13 @@ namespace TexnoStore.Controllers
         }
 
        
-        public override IActionResult ShopCartbyId(int Id)
+        public  IActionResult ShopCartbyId(int Id)
         {
             ShopCartMapper shopCartMapper = new ShopCartMapper();
 
             return View("LaptopProduct");
         }
 
-        public AllProductsListViewModel Checkout()
-        {
-            var userid = db.LoginRepository.Get(User.Identity.Name);
-            var user = db.ShopCartRepository.GetAll(userid.Id);
-            var laptops = db.LaptopRepository.Laptops();
-            var laptopModel = LaptopsModels(laptops);
-            var phones = db.PhoneRepository.Phones();
-            var phonesModel = PhoneModels(phones);
-
-            AllProductsListViewModel shopcartproducts = new AllProductsListViewModel();
-
-            var laptoplist = new List<LaptopModel>();
-            var phonelist = new List<PhoneModel>();
-
-            foreach (var a in user)
-            {
-                laptoplist.Add(laptopModel.FirstOrDefault(x => x.Id == a.LaptopId));
-                phonelist.Add(phonesModel.FirstOrDefault(x => x.Id == a.PhoneId));
-            }
-            shopcartproducts.LaptopModel = laptoplist;
-            shopcartproducts.PhoneModel = phonelist;
-            return shopcartproducts;
-        }
-
-        public List<LaptopModel> LaptopsModels(List<Laptop> laptops)
-        {
-            LaptopMapper laptopMapper = new LaptopMapper();
-            List<LaptopModel> laptopsModels = new List<LaptopModel>();
-
-            for (int i = 0; i < laptops.Count; i++)
-            {
-                var laptop = laptops[i];
-                var laptopModel = laptopMapper.Map(laptop);
-
-                laptopsModels.Add(laptopModel);
-            }
-
-            return laptopsModels;
-        }
-        public List<PhoneModel> PhoneModels(List<Phone> phones)
-        {
-            PhoneMapper phoneMapper = new PhoneMapper();
-            List<PhoneModel> phonesModels = new List<PhoneModel>();
-
-            for (int i = 0; i < phones.Count; i++)
-            {
-                var phone = phones[i];
-                var phoneModel = phoneMapper.Map(phone);
-
-                phonesModels.Add(phoneModel);
-            }
-
-            return phonesModels;
-        }
-
+      
     }
 }
