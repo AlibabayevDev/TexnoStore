3874a9b3d0d258abefd1323390574faa273cddec
Nahid Mirzazada <Admin@NAHIDPC>
2022-08-25 19:28:59 +0400

    controller added

---
+++ TexnoStoreApi/Controllers/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TexnoStoreWebCore.Services.Abstract;
+
+namespace TexnoStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CameraController : ControllerBase
+    {
+        private readonly ICameraService cameraService;
+        public CameraController(ICameraService cameraService)
+        {
+            this.cameraService = cameraService;
+        }
+
+        [HttpGet] 
+        public IActionResult Get()
+        {
+            try
+            {
+                var cameras = cameraService.Cameras();
+                if(cameras == null)
+                    return BadRequest("Something went wrong");
+
+                return Ok(cameras);
+            }
+            catch
+            {
+                BadRequest("something went wrong");
+            }
+
+            return BadRequest("something went wrong");
+        }
+
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var camera = cameraService.CameraById(id);
+                if (camera == null)
+                    return BadRequest("Data not found");
+
+                return Ok(camera);
+            }
+            catch
+            {
+                return BadRequest("Someting went wrong");
+            }
+        }
+
+    }
+}
