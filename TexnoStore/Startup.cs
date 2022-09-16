739144cf97c5a24e11f344734d4b70708a5cbabe
Nahid Mirzazada <Admin@NAHIDPC>
2022-09-16 13:41:01 +0400

    .

--- TexnoStore/Startup.cs
+++ TexnoStore/Startup.cs
@@ -111,7 +111,7 @@ namespace TexnoStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Account}/{action=Index}/{id?}");
+                    pattern: "{controller=SendAttachment}/{action=Index}/{id?}");
             });
         }
     }
